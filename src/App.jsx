a543b30064119a54314266fe2d882e2006038462
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-09-15 10:56:18 +0500

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,56 @@ import {
   CardMedia,
   CssBaseline,
   Grid,
-  Toolbar ,
+  Toolbar,
   Container,
+  ButtonGroup,
+  Button 
 } from "@mui/material";
-import {CameraAlt} from "@mui/icons-material";
+import { CameraAlt } from "@mui/icons-material";
 function App() {
   return (
-  <>
-  <CssBaseline/>
-  <AppBar position="relative">
-    <Toolbar >
-      <CameraAlt/>
-      <Typography variant='h6'>
-        PhotoAlbum
-      </Typography>
-    </Toolbar >
-  </AppBar>
-  </>)
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraAlt />
+          <Typography variant="h6">PhotoAlbum</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is photoAlbum and I am trying to make this
+              paragraph as long as possible so we can see how does it look like
+              on to the screeen
+            </Typography>
+          </Container>
+        </div>
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+      </main>
+    </>
+  );
 }
 
 export default App;
